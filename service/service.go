2428d49088bc9fe67aa73949cbb999398085a0aa
rajesh <C-rajasekar.sekaran@charter.com>
2019-01-24 18:48:18 -0500

    Renamed main.go to service.go

---
+++ service/service.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"github.com/rajesh81-sekaran/search"
+)
+
+func init() {
+	flag.Parse()
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+}
+
+func main() {
+	search.InitEnv()
+	search.InitCmdLine()
+	search.InitTradingDataUrl()
+	http.HandleFunc("/stock/", search.HandleStock)
+	log.Printf("starting service on port %s\n", search.SVCPort)
+	http.ListenAndServe(":"+search.SVCPort, nil)
+}
